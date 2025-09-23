494bb6ba71b83b6b4a9ae874b2a2c1580a25caaa
qai1 <dhiazalfazpoetro@gmail.com>
2025-09-23 11:21:20 +0700

    Adding Database Connection

---
+++ backend/src/config/db.js
@@ -0,0 +1,19 @@
+import mysql2 from "mysql2/promise";
+
+const pool = mysql2.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "notes_app",
+});
+
+export const testConnection = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Database connected successfully");
+    connection.release();
+  } catch (error) {
+    console.error("Database connection failed:");
+    throw error;
+  }
+};
